98fb0fbf773736cf7d129cab7da2ab964816312c
jasonkartz7 <69835301+jasonkartz7@users.noreply.github.com>
2023-02-25 14:04:04 +0800

    product list

---
+++ components/ProductItem.js
@@ -0,0 +1,31 @@
+/* eslint-disable @next/next/no-img-element */
+
+import Link from "next/link";
+import React from "react";
+
+export default function ProductItem(product) {
+  console.log(product.product.image);
+  return (
+    <div className="card">
+      <Link href={`/product/${product.product.slug}`}>
+        <img
+          className="rounded shadow"
+          src="{product.image}"
+          alt={product.product.name}
+          //   width={1000}
+          //   height={1000}
+        />
+      </Link>
+      <div className="flex flex-col items-center justify-center p-5">
+        <Link href={`/product/${product.product.slug}`}>
+          <h2 className="text-lg">{product.product.name}</h2>
+        </Link>
+        <p className="mb-2">{product.product.brand}</p>
+        <p>${product.product.price}</p>
+        <button className="primary-button" type="button">
+          Add To Cart
+        </button>
+      </div>
+    </div>
+  );
+}
