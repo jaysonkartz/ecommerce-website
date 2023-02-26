ea33c70382cd3dc19782650732f57bac1c2c2d61
jasonkartz7 <69835301+jasonkartz7@users.noreply.github.com>
2023-02-26 22:10:00 +0800

    save cart item

--- components/Layout.js
+++ components/Layout.js
@@ -2,11 +2,15 @@
 import { Store } from "@/utils/store";
 import Head from "next/head";
 import Link from "next/link";
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 
 export default function Layout({ title, children }) {
   const { state, dispatch } = useContext(Store);
   const { cart } = state;
+  const [cartItemsCount, setCartItemsCount] = useState(0);
+  useEffect(() => {
+    setCartItemsCount(cart.cartItems.reduce((a, c) => a + c.quantity, 0));
+  }, [cart.cartItems]);
   return (
     <>
       <Head>
@@ -27,7 +31,7 @@ export default function Layout({ title, children }) {
                 Cart{" "}
                 {cart.cartItems.length > 0 && (
                   <span className="ml-1 rounded-full bg-red-600 px-2 py-1 text-xs font-bold text-white">
-                    {cart.cartItems.reduce((a, c) => a + c.quantity, 0)}
+                    {cartItemsCount}
                   </span>
                 )}
               </Link>
