4bad62386de3f01e1b95119d45cbae2cdc0997bf
jasonkartz7 <69835301+jasonkartz7@users.noreply.github.com>
2023-02-26 14:00:46 +0800

    product detail

---
+++ pages/product/[slug].js
@@ -0,0 +1,58 @@
+/* eslint-disable @next/next/no-img-element */
+import { useRouter } from "next/router";
+import React from "react";
+import Layout from "@/components/Layout";
+import data from "@/utils/data";
+import Link from "next/link";
+
+export default function ProductScreen() {
+  const { query } = useRouter();
+  const { slug } = query;
+  const product = data.products.find((x) => x.slug === slug);
+  if (!product) {
+    return <Layout title="Product Not Found">Product Not Found</Layout>;
+  }
+
+  return (
+    <Layout title={product.name}>
+      <div className="py-2">
+        <Link href="/">Back to products</Link>
+      </div>
+      <div className="grid md:grid-cols-4 md:gap-3">
+        <div className="md:col-span-2">
+          <img
+            className="rounded shadow"
+            src={product.image}
+            alt={product.name}
+            width={640}
+            height={640}
+          />
+        </div>
+        <div>
+          <ul>
+            <li>
+              <h1 className="text-lg">{product.name}</h1>
+            </li>
+            <li>Category: {product.category}</li>
+            <li>Brand: {product.brand}</li>
+            <li>
+              {product.rating} of {product.numReviews} reviews
+            </li>
+            <li>Description: {product.description}</li>
+          </ul>
+        </div>
+        <div className="card p-5">
+          <div className="mb-2 flex justify-between">
+            <div>Price</div>
+            <div>${product.price}</div>
+          </div>
+          <div className="mb-2 flex justify-between">
+            <div>Status</div>
+            <div>{product.countInStock > 0 ? "In Stock" : "Unavailable"}</div>
+          </div>
+          <button className="primary-button w-full">Add to Cart</button>
+        </div>
+      </div>
+    </Layout>
+  );
+}
