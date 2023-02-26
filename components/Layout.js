29c93fd303ff717ba9010fe5ce22188b841939b5
jasonkartz7 <69835301+jasonkartz7@users.noreply.github.com>
2023-02-26 14:50:15 +0800

    handleCartAction

--- components/Layout.js
+++ components/Layout.js
@@ -1,8 +1,11 @@
+import { Store } from "@/utils/store";
 import Head from "next/head";
 import Link from "next/link";
-import React from "react";
+import React, { useContext } from "react";
 
 export default function Layout({ title, children }) {
+  const { state, dispatch } = useContext(Store);
+  const { cart } = state;
   return (
     <>
       <Head>
@@ -20,7 +23,12 @@ export default function Layout({ title, children }) {
             </Link>
             <div>
               <Link href="/cart" className="p-2">
-                Cart
+                Cart{" "}
+                {cart.cartItems.length > 0 && (
+                  <span className="ml-1 rounded-full bg-red-600 px-2 py-1 text-xs font-bold text-white">
+                    {cart.cartItems.reduce((a, c) => a + c.quantity, 0)}
+                  </span>
+                )}
               </Link>
               <Link href="/login" className="p-2">
                 Login
