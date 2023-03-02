3da5f1b1f5a561fefba9b44d8aaa63f450c3a2a6
jasonkartz7 <69835301+jasonkartz7@users.noreply.github.com>
2023-03-02 15:20:42 +0800

    loginAPI

--- components/Layout.js
+++ components/Layout.js
@@ -1,10 +1,15 @@
 /* eslint-disable no-unused-vars */
 import { Store } from "@/utils/store";
+import { useSession } from "next-auth/react";
 import Head from "next/head";
 import Link from "next/link";
 import React, { useContext, useEffect, useState } from "react";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function Layout({ title, children }) {
+  const { status, data: session } = useSession();
+
   const { state, dispatch } = useContext(Store);
   const { cart } = state;
   const [cartItemsCount, setCartItemsCount] = useState(0);
@@ -19,6 +24,8 @@ export default function Layout({ title, children }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
+      <ToastContainer position="bottom-center" limit={1} />
 
       <div className="flex min-h-screen flex-col justify-between">
         <header>
@@ -35,9 +42,16 @@ export default function Layout({ title, children }) {
                   </span>
                 )}
               </Link>
-              <Link href="/login" className="p-2">
-                Login
-              </Link>
+
+              {status === "loading" ? (
+                "Loading"
+              ) : session?.user ? (
+                session.user.name
+              ) : (
+                <Link href="/login" className="p-2">
+                  Login
+                </Link>
+              )}
             </div>
           </nav>
         </header>
