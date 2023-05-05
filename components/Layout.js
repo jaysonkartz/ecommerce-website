621af4cd8566205784fdd3ce23d7f73857f005c8
jasonkartz7 <69835301+jasonkartz7@users.noreply.github.com>
2023-05-05 15:16:07 +0800

    search

--- components/Layout.js
+++ components/Layout.js
@@ -9,6 +9,8 @@ import "react-toastify/dist/ReactToastify.css";
 import { Menu } from "@headlessui/react";
 import DropdownLink from "./DropdownLink";
 import Cookies from "js-cookie";
+import { SearchIcon } from "@heroicons/react/outline";
+import { useRouter } from "next/router";
 
 export default function Layout({ title, children }) {
   const { status, data: session } = useSession();
@@ -25,6 +27,15 @@ export default function Layout({ title, children }) {
     dispatch({ type: "CART_RESET" });
     signOut({ callbackUrl: "/login" });
   };
+
+  const [query, setQuery] = useState("");
+
+  const router = useRouter();
+  const submitHandler = (e) => {
+    e.preventDefault();
+    router.push(`/search?query=${query}`);
+  };
+
   return (
     <>
       <Head>
@@ -45,6 +56,24 @@ export default function Layout({ title, children }) {
             >
               EJSTUDIO
             </Link>
+            <form
+              onSubmit={submitHandler}
+              className="mx-auto  hidden w-full justify-center md:flex"
+            >
+              <input
+                onChange={(e) => setQuery(e.target.value)}
+                type="text"
+                className="rounded-tr-none rounded-br-none p-1 text-sm   focus:ring-0"
+                placeholder="Search products"
+              />
+              <button
+                className="rounded rounded-tl-none rounded-bl-none bg-amber-300 p-1 text-sm dark:text-black"
+                type="submit"
+                id="button-addon2"
+              >
+                <SearchIcon className="h-5 w-5"></SearchIcon>
+              </button>
+            </form>
             <div>
               <Link
                 href="/cart"
